6bc6df028be1ed629a9f7be6632e83f4900ff18b
vincebreezy <vdionisio22@gmail.com>
2024-04-26 22:48:14 -0700

    more css changes

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Role based access control project to simulate the SJSU Library website using Fla
 1. `git clone https://github.com/vincebreezy/cmpe132project`
 2. `pip install -e .`
 3. `flask --app website init-db`
-4. `sqlite3 < ./tests/prefill-db.sql`
+4. `sqlite3 < ./tests/premadelogins.db`
 5. Start website: `flask --app website run`
 6. Website can be found on http://127.0.0.1:5000
 
--- tests/premadelogins.sql
+++ tests/premadelogins.sql
@@ -3,18 +3,21 @@
 .output ./tests/results.json
 .open ./instance/website.sqlite
 
+--premade logins
 INSERT INTO users (first_name, last_name, username, password)
 VALUES ('Happy', 'Pancakes', '1hp', 'scrypt:32768:8:1$ESqrRwiTqHn72iPD$eef7c3a3af7a9c1be8d7c688bfacf96439b32dd45eaf93cb3f80e6e6a7980e3d93f5dccff5cf52eb41be3b03991518a01e2a0a39966df4ae40b5cbd587da422c'),
        ('iayze', 'iayze', 'iayze', 'scrypt:32768:8:1$Fh9Bada1Tkc9wyBw$fcd9aeb4cb1222a94e2c7690cebf524dfd3b16ec89ea2f99e218a8838645a189776151543dedfc62a93a832cb0ef86dd28c1b3076a8346a697048af9c321425e'),
        ('Ken', 'Carson', 'user00xman', 'scrypt:32768:8:1$J8t5fX5Y7kp2w4kB$e281a0cdca21283d08f54e4474e44109ab8e8e317f90f3856b600321f0491ec6ddf56cfaf0394c0b5b13a3c5b3b25ba5d5d95fd0ac1888bdc574fd70828218d5'),
        ('summrs', 'rino', 'summrsxo', 'scrypt:32768:8:1$vKyTB3XwQdt9qySm$0779c643921ba529b7dc009243ef89e0a1959e1e04258b2461d1cacb56acdac7ddc98a4953f84463ccf5e52966a17340374d6d0e74c93c65781d38df3fd3067e');
 
+--premade login roles
 INSERT INTO roles (role_name, username, approval)
 VALUES ('student', '1hp', 1),
        ('librarian', 'iayze', 0),
        ('admin', 'user00xman', 1),
        ('librarian', 'summrsxo', 1);
 
+--books
 INSERT INTO books (isbn, title, author, quantity)
 VALUES
     ('000-0-000-00000-1', 'Diary of a Wimpy Kid', 'Jeff Kinney', 5),
@@ -38,6 +41,7 @@ VALUES
     ('100-0-000-00000-9', 'Self Titled', 'Playboi Carti', 7),
     ('100-0-000-00000-0', 'NOSTYLIST', 'Destroy Lonely', 4);
 
+--room numbers
 INSERT INTO rooms (room_num) 
 VALUES
     (1),
